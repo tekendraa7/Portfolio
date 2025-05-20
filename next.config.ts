439044ad299071df9996f0248302bdf74924df51
Tekendra Bhandari <tekendrabhandari68@gmail.com>
2025-05-20 01:13:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Unhandled Runtime Error: Error: Invalid src prop (https://avatars.githubusercontent.com/u/182990458?v=4) on `next/image`, hostname "avatars.githubusercontent.com" is not configured under images in your `next.config.js`
    > See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host. Error source: src/components/portfolio/AboutSection.tsx (17:13) @ AboutSection
    >
    >   15 |         <div className="md:w-1/3 relative min-h-[300px] md:min-h-full">
    >   16 |           {data.profileImageUrl && (
    > > 17 |             <Image
    >      |             ^
    >   18 |               src={data.profileImageUrl}
    >   19 |               alt={data.name}
    >   20 |               layout="fill"
    >
    > Call Stack
    > 8
    >
    > Show 5 ignore-listed frame(s)
    > Array.map
    > <anonymous> (0:0)
    > AboutSection
    > src/components/portfolio/AboutSection.tsx (17:13)
    > PortfolioPage
    > src/app/portfolio/page.tsx (11:7)

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
